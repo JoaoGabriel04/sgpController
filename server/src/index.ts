0d38184c04dc91bff21c8fc81405a27211b181f4
JoaoGabriel04 <jgabrielcastro04@gmail.com>
2025-10-16 14:12:11 -0300

    SGP v1.0.5

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,21 @@
-import "dotenv/config"
-import express from "express"
-import cors from "cors"
-import apiRouter from "./api/routes"
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import apiRouter from "./api/routes";
 
-const PORT = process.env.PORT || 7000
-const app = express()
+const PORT = process.env.PORT || 7000;
+const app = express();
 
-app.use(cors(
-  {
-    origin: "*"
-  }
-))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(
+  cors({
+    origin: "https://sgpcontroller.netlify.app/",
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", apiRouter)
+app.use("/api", apiRouter);
 
-app.listen(PORT, ()=>{
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
