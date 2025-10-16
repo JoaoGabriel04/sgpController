68b09e79c920d8b44550122e65889cd7083ecaa8
JoaoGabriel04 <jgabrielcastro04@gmail.com>
2025-10-16 14:16:16 -0300

    SGP v1.0.6

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRouter);
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+app.listen(PORT, ()=>{
+  console.log(`Servidor rodando na porta ${PORT}!`)
+})
